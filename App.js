b94b229fc0c0aa4a683fbedf835f0447f544fd62
pedro melo <phmc_1995@hotmail.com>
2022-03-14 20:54:29 -0300

    adicionado navigationContainer p/ navegar entre telas

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as React from 'react';
 import Singin from './pages/Signin';
+//import Cadastro from './pages/Cadastro';
+
+//Navigation import
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <>
+    <NavigationContainer>
+      <>
       <Singin/>
       <StatusBar style="light" />
-    </>
+      </>
+    </NavigationContainer>
   );
 }
