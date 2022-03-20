451b0b9735049290c8e3e1b55f528782c2b07c60
pedro melo <phmc_1995@hotmail.com>
2022-03-20 17:27:03 -0300

    tela menu quase finalizada

--- pages/Menu/index.js
+++ pages/Menu/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native'
+import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native'
 import { Container } from './styles';
 
 const styles = StyleSheet.create({
@@ -14,7 +14,11 @@ const styles = StyleSheet.create({
 function Menu(){
     return(
         <Container>
-            <View style={styles.container}>
+            <View style={{
+                backgroundColor: '#5a58d4', //alterar dps
+                flex: 1,
+                alignItems: 'center'
+            }}>
                 <Image 
                     source={require('../../assets/images/logo.png')}
                     style={{resizeMode: 'contain',
@@ -23,22 +27,45 @@ function Menu(){
                     }}
                 />
                 <Text>MENU</Text>
-                <Image 
-                    source={require('../../assets/images/diet.png')}
-                    style={{resizeMode: 'contain',
-                            height: 200,
-                            width: 100
-                    }}
-                />
-                <Text>Dieta</Text>
-                <Image 
-                    source={require('../../assets/images/balance.png')}
-                    style={{resizeMode: 'contain',
-                            height: 200,
-                            width: 100
-                    }}
-                />
-                <Text>Calculadora IMC</Text>
+                <View style={{
+                    flex: 1, 
+                    flexDirection: 'row',
+                    alignContent: 'center'
+                }}>
+                    <TouchableOpacity
+                    style={styles.buttonFacebookStyle}
+                    activeOpacity={0.5}>
+                        <Image
+                        source={require('../../assets/images/diet.png')}
+                        style={{resizeMode: 'contain',
+                                height: 100,
+                                width: 70,
+                        }}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                    style={styles.buttonFacebookStyle}
+                    activeOpacity={0.5}>
+                        <Image
+                        source={require('../../assets/images/balance.png')}
+                        style={{resizeMode: 'contain',
+                                height: 100,
+                                width: 60,
+                        }}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                    style={styles.buttonFacebookStyle}
+                    activeOpacity={0.5}>
+                        <Image
+                        source={require('../../assets/images/metas.png')}
+                        style={{resizeMode: 'contain',
+                                height: 100,
+                                width: 60,
+                        }}
+                        />
+                    </TouchableOpacity>
+                </View>
             </View>
         </Container>
     )
