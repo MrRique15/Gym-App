561b5e0d49c7d4ba51045c2e5afe9b2be4863b15
Danilloddn <danillodiasnascimento@gmail.com>
2022-05-01 15:25:25 -0300

    Adicionando meta de peso desejada

--- pages/Metas/index.js
+++ pages/Metas/index.js
@@ -8,9 +8,17 @@ function Metas() {
       <View >
         <Image
           style={styles.icon}
-          source={require('../../assets/images/balance2.png')}
+          source={require('../../assets/images/metas.png')}
         />
       </View>
+      <Text style={styles.text}>Informe sua meta de peso</Text>
+      <TextInput
+        style={styles.input}
+        placeholderTextColor="#fff"
+        placeholder="peso desejado"
+        keyboardType="numeric"
+      />
+      
     </View>
   )
 }
@@ -20,13 +28,45 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: -100,
+    marginTop: -200,
     backgroundColor: 'rgb(90, 88, 212)',
   },
   icon: {
     resizeMode: 'contain',
-    height: 80,
+    height: 60,
     width: 300,
+  },
+  text: {
+    color: '#ffffff',
+    textAlign: 'center',
+    fontSize: 18,
+    height: 50,
+    marginTop: 30,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'rgb(90, 69, 161)',
+    borderRadius: 4,
+    borderRadius: 9,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    color: '#fff',
+    fontSize: 16,
+    width: '80%'
+  },
+  button: {
+    backgroundColor: 'rgb(90, 69, 161)',
+    borderRadius: 9,
+    width: '30%',
+    paddingVertical: 15,
+    alignItems: 'center',
+    marginTop: 30,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold'
   }
 });
 
